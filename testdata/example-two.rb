659a74494642d18b035dbca8bd43688a62a0b8a5
Victor Popkov <victor@popkov.me>
2017-07-31 16:47:03 +0300

    Add example-two testdata

---
+++ testdata/example-two.rb
@@ -0,0 +1,26 @@
+cask 'example-two' do
+  if MacOS.release <= :el_capitan
+    version '1.5.0'
+    sha256 '1f4dc096d58f7d21e3875671aee6f29b120ab84218fa47db2cb53bc9eb5b4dac'
+
+    url "https://example.com/app_#{version}.pkg"
+    appcast "https://example.com/sparkle/#{version}/el_capitan.xml",
+            checkpoint: '93ef3101ca730028d70524f71b7f6f17cbdb8d26906299f90c38b7079e1d03a4'
+  else
+    version '2.0.0'
+    sha256 'f22abd6773ab232869321ad4b1e47ac0c908febf4f3a2bd10c8066140f741261'
+
+    url "https://example.com/app_#{version}.pkg"
+    appcast "https://example.com/sparkle/#{version.major}/appcast.xml",
+            checkpoint: '57956bd3fb23a5673e30dc83ed19d51b43e5a9235756887f3ed90662e6c68fb7'
+  end
+
+  name 'Example'
+  name 'Example Two'
+  homepage 'https://example.com/'
+  license :commercial
+
+  pkg "app_#{version}.pkg"
+
+  uninstall pkgutil: 'com.example.pkg.*'
+end

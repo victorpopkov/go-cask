6bb2965510d7c7f9fbd96601f3c69bf7eb009d3f
Victor Popkov <victor@popkov.me>
2018-06-12 23:39:46 +0300

    Remove Appcast.Checkpoint

--- stanza.go
+++ stanza.go
@@ -65,17 +65,13 @@ type Appcast struct {
 
 	// URL specifies the appcast URL.
 	URL string
-
-	// Checkpoint specifies the checksum of the request response.
-	Checkpoint string
 }
 
 // NewAppcast creates a new Appcast instance and returns its pointer. Requires
 // both Appcast.URL and Appcast.Checkpoint to be passed as arguments.
 func NewAppcast(url string, checkpoint string) *Appcast {
 	return &Appcast{
-		URL:        url,
-		Checkpoint: checkpoint,
+		URL: url,
 	}
 }
 
